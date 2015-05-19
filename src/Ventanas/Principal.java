21f13e1c1c5316ea691f0b49b7b1b9c15e06f3d3
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2015-05-19 17:16:07 -0300

    add botonres

--- src/Ventanas/Principal.java
+++ src/Ventanas/Principal.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
 
 public class Principal extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField esit_Puerto;
 
 	/**
 	 * Launch the application.
@@ -35,8 +41,30 @@ public class Principal extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 414, 240);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		esit_Puerto = new JTextField();
+		esit_Puerto.setHorizontalAlignment(SwingConstants.CENTER);
+		esit_Puerto.setText("9003");
+		esit_Puerto.setBounds(10, 11, 86, 20);
+		panel.add(esit_Puerto);
+		esit_Puerto.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 79, 394, 150);
+		panel.add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.setBounds(175, 32, 89, 23);
+		panel.add(btnStart);
 	}
-
 }
